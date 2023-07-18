374d90f0396da609785bf22f30e2b7a4cb60b153
Ilyas Nmili <ilyasnmili2021@gmail.com>
2023-07-18 09:53:44 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main : Entry point
+ * Description : Write a program that prints _putchar, followed by a new line.
+ * Return: On success 0.
+
+ */
+int main(void)
+
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+               _putchar(str[ch]);
+       _putchar('\n');
+      
+       return (0);       
+

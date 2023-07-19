1570309c55c60dc13560598408e8eb6800f1016a
Ilyas Nmili <ilyasnmili2021@gmail.com>
2023-07-19 09:28:29 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/**
+ * using puts
+ */
 int  main(void)
 {
-	printf(""Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }

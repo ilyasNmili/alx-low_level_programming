739c9a2c32c9c85af3d34825273a4c87f0cee372
ilyasNmili <ilyasnmili22@gmai.com>
2023-07-23 08:48:58 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print 0123456789
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	return (0);
+}

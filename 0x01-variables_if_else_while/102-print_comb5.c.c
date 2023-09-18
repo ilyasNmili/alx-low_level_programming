e151021443121f34daeb0be5813757ab2fe5402a
ilyasNmili <ilyasnmili2021@gmail.com>
2023-09-18 16:10:59 +0100

    a

---
+++ 0x01-variables_if_else_while/102-print_comb5.c.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print_comb3
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+	int b;
+
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			putchar('0' + a / 10);
+			putchar('0' + a % 10);
+			putchar(' ');
+			putchar('0' + b / 10);
+			putchar('0' + b % 10);
+			if (a != 98 || b != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}

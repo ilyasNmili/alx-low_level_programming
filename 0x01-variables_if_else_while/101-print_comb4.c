32776b24ea118fd337b1c534fab63fe87682df2a
ilyasNmili <ilyasnmili2021@gmail.com>
2023-09-18 14:52:08 +0100

    a

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j;
+	int k;
+
+	while (i <= 7)
+	{
+		j = i + 1;
+		while (j <= 8)
+		{
+			k = j + 1;
+			while (k <= 9)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}

2933f9d1c0c2e293547ef8135bfd8a1a33e75566
ilyasNmili <ilyasnmili2021@gmail.com>
2023-09-18 13:44:35 +0100

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print_comb
+ * Return : Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar('0' + i);
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	return (0);
+}

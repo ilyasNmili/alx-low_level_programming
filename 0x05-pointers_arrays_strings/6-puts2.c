7e408ca89f925f04bdf7f76db730a1ee0d3b969b
ilyasNmili <ilyasnmili2021@gmail.com>
2023-09-18 20:32:15 +0100

    a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - takes string
+ * Return: void
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+	while (*str)
+	{
+		putchar(*str);
+		str += 2;
+	}
+}

8f9f72d959e2aee9180901554a12675d2659e224
ilyasNmili <ilyasnmili2021@gmail.com>
2023-09-18 22:25:38 +0100

    a

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
 
 int _putchar(char c);
+
+/**
+ * _strstr - return string
+ * Return: string
+ * @haystack: string
+ * @needle: string to find
+ */
+
 char *_strstr(char *haystack, char *needle);
 
 #endif

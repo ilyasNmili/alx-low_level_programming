46faeb6e54cc08651ac1b5e8c252ddf9bf344ec3
ilyasNmili <ilyasnmili2021@gmail.com>
2023-09-20 16:36:12 +0100

    a

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - set_string
+ * Return: void
+ * @s: double pointer
+ * @to: pointer
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

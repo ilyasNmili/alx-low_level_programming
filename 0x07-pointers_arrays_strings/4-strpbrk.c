c038a0dbd194afd890e5b2987efb8bc95759644f
ilyasNmili <ilyasnmili2021@gmail.com>
2023-09-20 15:33:17 +0100

    a

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * strpbrk - return pointer
+ * Return: char pointer
+ * @s: string
+ * @accept: string
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j;
+
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+

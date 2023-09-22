a3e7e270ef3943fbfe76a438173ef3bbeaa3c716
ilyasNmili <ilyasnmili2021@gmail.com>
2023-09-22 17:55:11 +0100

    a

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - print_reverse
+ *Return: void
+ *@s: string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*(s + 1))
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar(*s);
+}

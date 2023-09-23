75c24addbbd460c46b1b94c09401ce2fb9bbd6ed
ilyasNmili <ilyasnmili2021@gmail.com>
2023-09-23 17:42:42 +0100

    a

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,17 +2,18 @@
 
 /**
  * main - program name
- * Return: void
+ * Return: 0 in Success
  *@ac: argc
  *@av: argv
  */
 
 int main(int ac, char **av)
 {
-	while (*av[0])
+	while (*av[0] && ac != 0)
 	{
 		_putchar(*av[0]);
 		av[0]++;
 	}
 	_putchar('\n');
+	return (0);
 }

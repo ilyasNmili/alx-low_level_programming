d594b9bd5c772f5d413846a80cc74a5926e87834
ilyasNmili <ilyasnmili2021@gmail.com>
2023-09-25 20:25:54 +0100

    a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,10 +11,11 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str = (char *)malloc(size * sizeof(char) + 1);
-	int i = 0;
+	unsigned int i = 0;
 
 	for (i = 0; i < size; i++)
 		str[i] = c;
 	str[i] = '\0';
 	return (str);
+
 }

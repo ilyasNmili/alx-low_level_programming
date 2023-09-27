fb3e446392116dae17756fa82644b58766406e88
ilyasNmili <ilyasnmili2021@gmail.com>
2023-09-27 21:15:25 +0100

    a

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _atoi - str_to_int
@@ -11,13 +12,15 @@ int _atoi(char *s)
 	int n = 0;
 	unsigned int signe = 1;
 
-	while (*s < '0' || *s > '9')
+	if (strcmp(s, "-2147483648") == 0)
+		return (-2147483648);
+	while (*s && (*s < '0' || *s > '9'))
 	{
 		if (*s == '-')
 			signe *= -1;
 		s++;
 	}
-	while (*s >= '0' && *s <= '9')
+	while (*s && *s >= '0' && *s <= '9')
 	{	n = n * 10 + *s - '0';
 		s++;
 	}

d62f2c317b3c8dc617b73448d95f83b2aaafacd7
ilyasNmili <ilyasnmili2021@gmail.com>
2023-10-02 23:37:41 +0100

    a

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _atoi - str_to_int
+ * Return: integer
+ * @s: string
+ */
+
+int _atoi(char *s)
+{
+	int n = 0;
+	int signe = 1;
+
+	if (strcmp(s, "-2147483648") == 0)
+		return (-2147483648);
+	while (*s && (*s < '0' || *s > '9'))
+	{
+		if (*s == '-')
+			signe *= -1;
+		s++;
+	}
+	while (*s && *s >= '0' && *s <= '9')
+	{	n = n * 10 + (*s - '0');
+		s++;
+	}
+	return (signe * n);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts -  take char pointer
+ * Return: void
+ * @str: string
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
+}

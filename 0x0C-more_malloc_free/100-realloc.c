3a166790eff8667f7490249f84486097b475c243
ilyasNmili <ilyasnmili2021@gmail.com>
2023-10-07 02:28:17 +0100

    a

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - check code
+ * Return: void
+ * @ptr: void pointer
+ * @old_size: unsigned int
+ * @new_size: unsigned int
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *ret;
+	char *ancien_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr)
+		return (NULL);
+	ret = malloc(new_size);
+	if (!ret)
+		return (NULL);
+	ancien_ptr = ptr;
+	if (new_size > old_size)
+		ret = ptr;
+	else
+	{
+		for (i = 0; i < new_size; i++)
+			ret[i] = ancien_ptr[i];
+	}
+	free(ptr);
+	return (ret);
+}

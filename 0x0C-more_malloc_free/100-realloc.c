cee4afc0c4f364214f08023c1c2a952f70b61d0e
ilyasNmili <ilyasnmili2021@gmail.com>
2023-10-07 02:38:54 +0100

    a

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,7 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr)
-	{	
+	{
 		free(ptr);
 		return (NULL);
 	}
@@ -27,7 +27,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	ancien_ptr = ptr;
 	if (new_size > old_size)
-		ret = (char *)ptr;
+	{
+		for (i = 0; i < old_size; i++)
+			ret[i] = ancien_ptr[i];
+	}
 	else
 	{
 		for (i = 0; i < new_size; i++)

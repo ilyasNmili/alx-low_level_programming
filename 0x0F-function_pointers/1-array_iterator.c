7501341e0c136c437ef567af941ddef0c30e49ca
ilyasNmili <ilyasnmili2021@gmail.com>
2023-10-12 01:25:22 +0100

    a

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - 
+ * @array: int pointer
+ * @size: int
+ * @action: function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}

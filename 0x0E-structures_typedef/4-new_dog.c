00184ebcc446c9e3d79ffdbfdf33b4799f2c4342
ilyasNmili <ilyasnmili2021@gmail.com>
2023-10-13 19:01:44 +0100

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,26 @@
 #include <stdlib.h>
 
 /**
+ * _strcpy - copy string
+ * Return: char pointer
+ * @dest: destination
+ * @src: source
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
  * _size - size
  * Return: int
  * @str: string
@@ -45,8 +65,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog->name);
 		return (NULL);
 	}
-	dog->name = name;
+	_strcpy(dog->name, name);
 	dog->age = age;
-	dog->owner = owner;
+	_strcpy(dog->owner, owner);
 	return (dog);
 }

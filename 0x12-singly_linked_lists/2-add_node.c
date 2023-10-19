d438d2d3d6f04009a6efac89f77e9c377a0058c7
ilyasNmili <ilyasnmili2021@gmail.com>
2023-10-19 19:58:25 +0100

    a

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * add_node - add node
+ * Return: list
+ * @head: list
+ * @str: string
+*/
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+	unsigned int size = 0;
+	list_t *newNode = malloc(sizeof(list_t));
+
+	newNode->str = strdup(str);
+	if (!newNode->str)
+		return (NULL);
+	while (str[size])
+		size++;
+	newNode->len = size;
+	newNode->next = *head;
+	*head = newNode;
+	return (newNode);
+}

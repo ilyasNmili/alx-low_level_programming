17f506714e629a6662ee0373d1ece3eae4ba8acb
ilyasNmili <ilyasnmili2021@gmail.com>
2023-10-19 16:11:59 +0100

    a

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,14 +12,12 @@ size_t print_list(const list_t *h)
 {
 	size_t size = 0;
 
-	if (!h)
-		return (0);
+
 	while (h != NULL)
 	{
 		if (h->str)
 		{
-			printf("[%u] ", h->len);
-			printf("%s\n", h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
 		else
 			printf("[0] (nil)\n");

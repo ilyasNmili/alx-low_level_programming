67ac34161914f93757b6bb10e512a479dd3d4a8d
ilyasNmili <ilyasnmili2021@gmail.com>
2023-10-19 19:25:30 +0100

    a

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
-
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_list - print_list
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
+
 #include <stdlib.h>
 
 void _putchar(char c);
